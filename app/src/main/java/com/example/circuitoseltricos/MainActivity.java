98916587aab37ad95f6837d74ab085056440138b
TulioSergio1 <tulio.goncalves@academico.ifpb.edu.br>
2024-11-07 12:17:12 -0300

    Atualizações da interface

--- app/src/main/java/com/example/circuitoseltricos/MainActivity.java
+++ app/src/main/java/com/example/circuitoseltricos/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         substituirFragment(new HomeFragment());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, 0, systemBars.right, 0);
             return insets;
         });
 
